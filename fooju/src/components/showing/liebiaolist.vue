0cb69ec2993413522aa003b0f52eb6fbdbdf8de9
MissWu555 <1312127106@qq.com>
2017-09-25 08:31:46 +0800

    wuqiaoning

--- fooju/src/components/showing/liebiaolist.vue
+++ fooju/src/components/showing/liebiaolist.vue
@@ -44,13 +44,13 @@
       <!-- tabcontainer -->
       <mt-tab-container v-model="selected">
         <mt-tab-container-item id="1">
-          <mt-cell v-for="n in 10" :title="'内容 ' + n" />
+          <mt-cell v-for="n in 10" :key="10" :title="'内容 ' + n" />
         </mt-tab-container-item>
         <mt-tab-container-item id='2'>
-          <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+          <mt-cell v-for="n in 4" :key="4" :title="'测试 ' + n" />
         </mt-tab-container-item>
         <mt-tab-container-item id='3'>
-          <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+          <mt-cell v-for="n in 6" :key="6" :title="'选项 ' + n" />
         </mt-tab-container-item>
       </mt-tab-container>
     </mt-popup>
